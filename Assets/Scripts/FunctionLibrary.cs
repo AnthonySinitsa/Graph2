f228c94b290f70c0a3f3313172e806265481e72d
Anton <itonyemail@gmail.com>
2023-12-18 13:44:54 -0800

    created delegate function

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -3,6 +3,19 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
 
+    public delegate float Function(float x, float y);
+    public static Function GetFunction(int index){
+        if (index == 0) {
+			return Wave;
+		}
+		else if (index == 1) {
+			return MultiWave;
+		}
+		else {
+			return Ripple;
+		}
+    }
+
     public static float Wave(float x, float t){
         return Sin(PI * (x + t));
     }
