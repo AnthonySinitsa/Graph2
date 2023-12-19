a9c21474706f41b15e85c5966f25c4079b1241d7
Anton <itonyemail@gmail.com>
2023-12-18 23:53:54 -0800

    creating spheere

--- Assets/Scripts/FunctionLibrary.cs
+++ Assets/Scripts/FunctionLibrary.cs
@@ -5,9 +5,9 @@ public static class FunctionLibrary{
 
     public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName {Wave, MultiWave, Ripple}
+    public enum FunctionName {Wave, MultiWave, Ripple, Sphere}
 
-    static Function[] functions = {Wave, MultiWave, Ripple};
+    static Function[] functions = {Wave, MultiWave, Ripple, Sphere};
 
     public static Function GetFunction(FunctionName name){
         return functions[(int)name];
@@ -41,4 +41,14 @@ public static class FunctionLibrary{
 		p.z = v;
 		return p;
 	}
+
+    public static Vector3 Sphere(float u, float v, float t){
+        float r = 0.9f + 0.1f * Sin(8f * PI * u);
+        float s = r * Cos(0.5f * PI * v);
+        Vector3 p;
+        p.x = s * Sin(PI * u);
+        p.y = r * Sin(PI * 0.5f * v);
+        p.z = s * Cos(PI * u);
+        return p;
+    }
 }
