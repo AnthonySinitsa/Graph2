aed2c48be0ea54f2b5a66e0e95b7c3dbbaa2ea05
Anton <itonyemail@gmail.com>
2023-12-20 00:20:18 -0800

    Update Graph.cs

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -65,6 +65,22 @@ public class Graph : MonoBehaviour {
 			FunctionLibrary.GetRandomFunctionNameOtherThan(function);
 	}
 
+	void UpdateFunction () {
+	FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+	float time = Time.time;
+	float step = 2f / resolution;
+	float v = 0.5f * step - 1f;
+	for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+		if (x == resolution) {
+			x = 0;
+			z += 1;
+			v = (z + 0.5f) * step - 1f;
+		}
+		float u = (x + 0.5f) * step - 1f;
+		points[i].localPosition = f(u, v, time);
+	}
+}
+
 	void UpdateFunctionTransition() {
 		FunctionLibrary.Function 
 			from = FunctionLibrary.GetFunction(transitionFunction),
