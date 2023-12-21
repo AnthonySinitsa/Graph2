91a4e0413f3b8e8d68281ceb3ad41a04af57e5f0
Anton <itonyemail@gmail.com>
2023-12-21 14:52:38 -0800

    Update GPUGraph.cs

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class GPUGraph : MonoBehaviour {
 
+	static readonly int 
+    positionsId = Shader.PropertyToID("_Positions"),
+    resolutionId = Shader.PropertyToID("_Resolution"),
+    stepId = Shader.PropertyToID("_Step"),
+    timeId = Shader.PropertyToID("_Time"); 
+
 	[SerializeField]
 	ComputeShader computeShader;
 
@@ -50,11 +56,24 @@ public class GPUGraph : MonoBehaviour {
 			transitionFunction = function;
 			PickNextFunction();
 		}
+		UpdateFunctionOnGPU();
 	}
 
 	void PickNextFunction(){
 		function = transitionMode == TransitionMode.Cycle ? 
 			FunctionLibrary.GetNextFunctionName(function) :
 			FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+	}
+
+	void UpdateFunctionOnGPU(){
+    float step = 2f / resolution;
+    computeShader.SetInt(resolutionId, resolution);
+    computeShader.SetFloat(stepId, step);
+    computeShader.SetFloat(timeId, Time.time);
+
+    computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+    int groups = Mathf.CeilToInt(resolution / 8f);
+    computeShader.Dispatch(0, groups, groups, 1);
 	}
 }
